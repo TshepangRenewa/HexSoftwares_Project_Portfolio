1de0562d3b616bf1aaa0347550e087a189f78f44
Kamogelo <ekmogasoa@gmail.com>
2024-12-06 13:18:10 +0200

    Moved js scroll logic into app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,4 @@
-alert("Its working all linked!");
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("scrolled", window.scrollY > 70);
+});
